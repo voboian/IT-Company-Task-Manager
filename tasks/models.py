d4e893699855a5fe6d06515ac4667b6205706f7f
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-01 14:58:50 +0200

    add base.html

--- tasks/models.py
+++ tasks/models.py
@@ -11,7 +11,7 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers")
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers", null=True, blank=True)
 
 
 class Task(models.Model):
