0309ec94d409902b52acdbe9976a29e7e4c964da
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-01 17:32:21 +0200

    add path for worker and task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from tasks.views import index, PositionListView, TaskTypeListView, WorkerListView, TaskListView
+from tasks.views import index, PositionListView, TaskTypeListView, WorkerListView, TaskListView, TaskDetailView, \
+    WorkerDetailView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -10,7 +11,9 @@ urlpatterns = [
     ),
     path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
     path("task/", TaskListView.as_view(), name="task-list"),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("worker/", WorkerListView.as_view(), name="worker-list"),
+    path("worker/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
 
 ]
 
