7664338d61a9321f04a1964fe031dfd1491c1f84
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-01 16:45:04 +0200

    add urls path and html codes on another 'lists'

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from tasks.views import index
+from tasks.views import index, PositionListView, TaskTypeListView, WorkerListView, TaskListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "position/",
+        PositionListView.as_view(),
+        name="position-list"
+    ),
+    path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("task/", TaskListView.as_view(), name="task-list"),
+    path("worker/", WorkerListView.as_view(), name="worker-list"),
 
 ]
 
