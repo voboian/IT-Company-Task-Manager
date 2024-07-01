e329164b55553a425fe7b04f2daa98e4204e5137
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-01 12:50:55 +0200

    add models, views, templates and create 'index' for startpage statistics

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from tasks.models import Task, Worker, Position, TaskType
+
+
+@login_required
+def index(request):
+    num_tasks = Task.objects.count()
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+    num_task_types = TaskType.objects.count()
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_task_types": num_task_types,
+    }
+
+    return render(request, "taskmanager/index.html", context=context)
+
+
+class TaskListView(ListView):
+    model = Task
