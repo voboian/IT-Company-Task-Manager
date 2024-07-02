f733d666ad193cfc240489f6bfcb57b910c038f4
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-02 18:22:03 +0200

    add query_transform in pagination

--- tasks/templatetags/query_transform.py
+++ tasks/templatetags/query_transform.py
@@ -4,13 +4,11 @@ register = template.Library()
 
 
 @register.simple_tag
-def query_transform(request, **kwargs) -> str:
+def query_transform(request, **kwargs):
     updated = request.GET.copy()
-
     for key, value in kwargs.items():
         if value is not None:
             updated[key] = value
         else:
             updated.pop(key, 0)
-
     return updated.urlencode()
