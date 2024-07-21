7a28d7cf3b2c357e6d1e64035246f39e52f53dd1
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-21 13:44:22 +0200

    update README.md and add url to ALLOWED_HOSTS

--- ITCompanyTaskManager/settings.py
+++ ITCompanyTaskManager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'it-company-task-manager-ic9q.onrender.com']
 
 # Application definition
 
